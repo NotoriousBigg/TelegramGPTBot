2abbe2133fc57b4da0daee90653ff2b57c73c31a
(^▽^) ＫＲＥＳＳＷＥＬＬ (✿^▽^) <163395551+Muiruri42@users.noreply.github.com>
2024-05-01 09:57:21 +0300

    Change the API to gemini

--- main.py
+++ main.py
@@ -20,18 +20,16 @@ Type /help or /start to get this message.
     bot.send_message(message.chat.id, startmess, reply_markup=keyboard)
 
 #Code for Answering to all messages
-@bot.message_handler(func=lambda message: True)
+#It uses the Gemini API
 def reply_gptmess(message):
     try:
-      #API PROVIDED BY NEPDEV CODERS
-        url = f"https://chatgpt.apinepdev.workers.dev/?question={message}"
-        response = requests.get(url)
-        if response.status_code == 200:
-            res_json = response.json()
-            answer = res_json.get("answer")
-            #bot.send_chat_action(message.cgat.id,"typing")
-            bot.send_message(message.chat.id, answer)
-        else:
+      url = f"https://dev-the-dark-lord0.pantheonsite.io/wp-admin/js/Apis/Gemini.php?message={message.text}"
+      response = requests.get(url)
+      if response.status_code == 200:
+        answer = response.text
+        #bot.send_chat_action(message.chat.id,"typing")
+        bot.send_message(message.chat.id, answer, parse_mode="Markdown")
+      else:
             bot.send_message(message.chat.id, "A fatal error occured. Please contact admin.")
     except Exception as e:
         bot.send_message(message.chat.id, f"The following error occured \n\n {e} .\nForward It to admin")
